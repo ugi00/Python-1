5d5c110c377aedb418f1e3a63007064554880e50
ugi00 <71213609+ugi00@users.noreply.github.com>
2021-05-23 17:06:10 +0900

    111

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from tkinter import *
 from tkinter import font
 import tkinter.ttk
+import Show
 
 class Main:
     window = Tk()
     Font = font.Font(window, size=15, weight='bold', family='Consolas')
+    FindShow=Show.Contents()
     def __init__(self):
 
         self.window.title("공연 전시 정보 검색")
@@ -22,54 +24,30 @@ class Main:
         button=Button(self.window,text='Gmail',image=self.GmailImage)
         button.pack()
         button.place(x=1200 - self.GmailImage.width(), y=100)
-#        PeriodScrollBar=Scrollbar(window)
-#        PeriodScrollBar.pack()
-#        PeriodScrollBar.place(x=850,y=120)
- #       TempFont= font.Font(window,size=30,weight='bold',family='Consolas')
-  #      SearchListBox=Listbox(window,font=TempFont,activestyle='none',
-   #                           width=15,height=1,borderwidth=1,relief='ridge',
-    #                          yscrollcommand=PeriodScrollBar.set)
-     #   SearchListBox.insert(1,"기간별 공연 검색")
-      #  SearchListBox.pack()
-       # SearchListBox.place(x=520,y=120)
-        #PeriodScrollBar.config(command=SearchListBox.yview)
-        #AreaScrollBar = Scrollbar(window)
-        #AreaScrollBar.pack()
-        #AreaScrollBar.place(x=850, y=50)
-        #TempFont = font.Font(window, size=30, weight='bold', family='Consolas')
-        #AreaSearchListBox = Listbox(window, font=TempFont, activestyle='none',
-        #                        width=15, height=1, borderwidth=1, relief='ridge',
-        #                        yscrollcommand=AreaScrollBar.set)
-        #AreaSearchListBox.insert(1, "지역별 공연 검색")
-        #AreaSearchListBox.pack()
-        #AreaSearchListBox.place(x=520, y=50)
-        #AreaScrollBar.config(command=AreaSearchListBox.yview)
+        self.PeriodEntry=Entry(self.window,font=self.Font,width=30,relief='ridge')
+        self.PeriodEntry.pack()
+        self.PeriodEntry.place(x=520,y=80)
+        self.PeriodButton=Button(self.window,font=self.Font,text="검색",command=self.FindShow.SearchPeriod)
+        self.PeriodButton.pack()
+        self.PeriodButton.place(x=870,y=80)
+        self.AreaEntry = Entry(self.window, font=self.Font, width=30, relief='ridge')
+        self.AreaEntry.pack()
+        self.AreaEntry.place(x=520, y=170)
+        self.AreaButton = Button(self.window, font=self.Font, text="검색", command=self.FindShow.SearchArea)
+        self.AreaButton.pack()
+        self.AreaButton.place(x=870, y=170)
+        self.releamEntry = Entry(self.window, font=self.Font, width=30, relief='ridge')
+        self.releamEntry.pack()
+        self.releamEntry.place(x=520, y=260)
 
-        #ReleamScrollBar = Scrollbar(window)
-        #ReleamScrollBar.pack()
-        #ReleamScrollBar.place(x=850, y=190)
-        #TempFont = font.Font(window, size=30, weight='bold', family='Consolas')
-        #ReleamSearchListBox = Listbox(window, font=TempFont, activestyle='none',
-        #                            width=15, height=1, borderwidth=1, relief='ridge',
-        #                            yscrollcommand=ReleamScrollBar.set)
-        #ReleamSearchListBox.insert(1, "분야별 공연 검색")
-        #ReleamSearchListBox.pack()
-        #ReleamSearchListBox.place(x=520, y=190)
-        #ReleamScrollBar.config(command=ReleamSearchListBox.yview)
-
-        #SearchResultScrb=Scrollbar(window)
-        #SearchResultScrb.pack()
-        #SearchResultScrb.place(x=850,y=260)
-        #TempFont=font.Font(window,size=10,family='Consolas')
-
-   #     SearchText=Text(window,font=TempFont,width=47,height=5,borderwidth=1,relief='ridge',
-    #                    yscrollcommand=SearchResultScrb.set)
-    #    SearchText.pack()
-    #    SearchText.place(x=520,y=260)
-    #    SearchResultScrb.config(command=SearchText.yview)
-
-#        SearchText.configure(state='disabled')
-
+        self.ResultScrollBar=Scrollbar(self.window)
+        self.ResultScrollBar.pack()
+        self.ResultScrollBar.place(x=870,y=350)
+        self.ResultText=Text(self.window,width=49,height=27,borderwidth=5,relief='ridge',yscrollcommand=self.ResultScrollBar.set)
+        self.ResultText.pack()
+        self.ResultText.place(x=520,y=350)
+        self.ResultScrollBar.config(command=self.ResultText.yview)
+        self.ResultText.configure(state='disabled')
         self.window.mainloop()
 
 Main()
